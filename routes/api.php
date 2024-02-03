c20a12a92dc0b674cc56caa97406362e3161bc16
gutaam.gt <gutaam.gt@gmail.com>
2024-02-03 16:11:54 +0700

    sd

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,18 @@ Route::post('/register', 'Api\AuthController@register');
 Route::get('/pembelajaran/cek-pertemuan', 'Api\PembelajaranController@cekPertemuan');  
 Route::post('/pembelajaran/store', 'Api\PembelajaranController@store');  
 Route::get('/pembelajaran', 'Api\PembelajaranController@index');  
+Route::get('/pembelajaran/list-pertemuan', 'Api\PembelajaranController@listPertemuan');  
 Route::post('/pembelajaran/update/{id}', 'Api\PembelajaranController@update');  
 Route::delete('/pembelajaran/delete/{id}', 'Api\PembelajaranController@delete');  
 
 Route::get('/data-mhs', 'Api\AkademikController@dataMhs'); 
 
 Route::get('/absensi', 'Api\AbsensiController@index');   
-Route::post('/absensi/store', 'Api\AbsensiController@store');  
+Route::post('/absensi/store', 'Api\AbsensiController@store'); 
+
+Route::get('/absensi/scanqr', 'Api\AbsensiController@scanQr');  
 Route::post('/absensi/scan-qr', 'Api\AbsensiController@scanQr'); 
+
 Route::get('/absensi/show-qr', 'Api\AbsensiController@showQr'); 
 Route::get('/absensi/download-qr', 'Api\AbsensiController@downloadQr'); 
 Route::post('/absensi/update/{id}', 'Api\AbsensiController@update');  
